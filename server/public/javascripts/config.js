9cb4cdffb300d4fbdec81e1a6783b1dac7d51cf0
liuzhen88 <l458861320@qq.com>
2016-06-18 21:53:06 +0800

    add goods shop index

--- server/public/javascripts/config.js
+++ server/public/javascripts/config.js
@@ -1,3 +1,3 @@
-// var localServerUrl = "http://localhost:3000";
-// var serverUrl = "http://121.43.57.75:3000";
-var serverUrl = "http://localhost:3000";
+var localServerUrl = "http://localhost:3000";
+var serverUrl = "http://121.43.57.75:3000";
+// var serverUrl = "http://localhost:3000";

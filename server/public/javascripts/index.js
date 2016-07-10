27fa24ac3397161af33c87760dd831045b2d12b0
liuzhen88 <l458861320@qq.com>
2016-07-10 18:06:08 +0800

    修改

--- server/public/javascripts/index.js
+++ server/public/javascripts/index.js
@@ -3,17 +3,59 @@ window.onload = function(){
 	var page = 1;
 	var app = {
 		init:function(){
-			var index = 1;
-			setInterval(function(){
-				 $(".banner-lists").hide();
-				 $(".banner-lists").eq(index).fadeIn();
-				 if(index == $(".banner-lists").length-1){
-				 	index = 0;
-				 }else{
-				 	index ++;
-				 }
-			},3000);
-
+			var len = $(".oUlplay li").length;
+			$(".oUlplay").width(len*1100);
+			  var oDiv = document.getElementById('playBox');
+			  var oPre = getByClass(oDiv,'pre')[0];
+			  var oNext = getByClass(oDiv,'next')[0];
+			  var oUlBig = getByClass(oDiv,'oUlplay')[0];
+			  var aBigLi = oUlBig.getElementsByTagName('li');
+			  var oDivSmall = getByClass(oDiv,'smalltitle')[0]
+			  var aLiSmall = oDivSmall.getElementsByTagName('li');
+			  function tab(){
+	     for(var i=0; i<aLiSmall.length; i++)
+	     {
+		    aLiSmall[i].className = '';
+	     }
+	     aLiSmall[now].className = 'thistitle'
+	     startMove(oUlBig,'left',-(now*aBigLi[0].offsetWidth))
+	  }
+	  var now = 0;
+	  for(var i=0; i<aLiSmall.length; i++){
+		  aLiSmall[i].index = i;
+		  aLiSmall[i].onclick = function()
+		  {
+			  now = this.index;
+			  tab();
+		  }
+	 }
+	  oPre.onclick = function()
+	  {
+		  now--
+		  if(now ==-1)
+		  {
+			  now = aBigLi.length;
+		  }
+		   tab();
+	  }
+	   oNext.onclick = function()
+	  {
+		   now++
+		  if(now ==aBigLi.length)
+		  {
+			  now = 0;
+		  }
+		  tab();
+	  }
+	  var timer = setInterval(oNext.onclick,5000); //滚动间隔时间设置
+	  oDiv.onmouseover = function()
+	  {
+		  clearInterval(timer);
+	  }
+	   oDiv.onmouseout = function()
+	  {
+		  timer = setInterval(oNext.onclick,5000); //滚动间隔时间设置
+	  }
 			//公告
 			$(".notice-list").click(function(){
 				var id = $(this).attr("data-id");
@@ -122,4 +164,61 @@ window.onload = function(){
 	}
 
 	app.init();
+}
+function getStyle(obj,name){
+	if(obj.currentStyle)
+	{
+		return obj.currentStyle[name]
+	}
+	else
+	{
+		return getComputedStyle(obj,false)[name]
+	}
+}
+
+function getByClass(oParent,nClass){
+	var eLe = oParent.getElementsByTagName('*');
+	var aRrent  = [];
+	for(var i=0; i<eLe.length; i++)
+	{
+		if(eLe[i].className == nClass)
+		{
+			aRrent.push(eLe[i]);
+		}
+	}
+	return aRrent;
+}
+
+function startMove(obj,att,add){
+	clearInterval(obj.timer)
+	obj.timer = setInterval(function(){
+	   var cutt = 0 ;
+	   if(att=='opacity')
+	   {
+		   cutt = Math.round(parseFloat(getStyle(obj,att)));
+	   }
+	   else
+	   {
+		   cutt = Math.round(parseInt(getStyle(obj,att)));
+	   }
+	   var speed = (add-cutt)/4;
+	   speed = speed>0?Math.ceil(speed):Math.floor(speed);
+	   if(cutt==add)
+	   {
+		   clearInterval(obj.timer)
+	   }
+	   else
+	   {
+		   if(att=='opacity')
+		   {
+			   obj.style.opacity = (cutt+speed)/100;
+			   obj.style.filter = 'alpha(opacity:'+(cutt+speed)+')';
+		   }
+		   else
+		   {
+			   obj.style[att] = cutt+speed+'px';
+		   }
+	   }
+	   
+	},30)
 }
--- server/staic/wapdev/js/class.js
+++ server/staic/wapdev/js/class.js
@@ -1,33 +1,98 @@
-var flag = 0;
+// var flag = 0;
+// $(document).ready(function(){
+// 	$.ajax({
+// 		url:serverUrl+"/users/getClassData",
+// 		type:"get",
+// 		dataType:"json",
+// 		success:function(data){
+// 			if(data.code==200){
+// 				var classDoTmpl = doT.template($("#class-tmpl").html());
+//                 $("#classBox").html(classDoTmpl(data.data));
+
+//                 $(".classList").click(function(){
+//                 	$(".classList").removeClass("has-select");
+//                 	$(this).addClass("has-select");
+//                 	var text = $(this).text();
+//                 	getClassListData(text);
+//                 });
+// 			}
+// 		},
+// 		error:function(err){
+// 			console.log(err);
+// 		}
+// 	});
+
+// 	$(".more").click(function(){
+// 		$(".all").toggle();
+// 	});
+
+// 	function getClassListData(text){
+// 		$(".all").hide();
+// 		$.ajax({
+// 			url:serverUrl+"/users/getClassListData",
+// 			type:"post",
+// 			data:{
+// 				text:text
+// 			},
+// 			dataType:"json",
+// 			json:"callback",
+// 			success:function(data){
+// 				if(data.code==200){
+// 					var goodsDoTmpl = doT.template($("#goods-tmpl").html());
+//                 	$("#goodsBox").html(goodsDoTmpl(data.data));
+
+     
+// 				}
+// 			},
+// 			error:function(err){
+// 				console.log(err);
+// 			}
+// 		});
+// 	}
+// });
+
+// function getDetail(obj){
+	
+// 	if(flag == 0){
+// 		var id = $(obj).attr("data-id");
+// 		window.location.href="class_detail.html?id="+id;
+// 	}
+// 	flag ++;
+    
+// }
+
 $(document).ready(function(){
+	var height = $(window).height();
+	$(".class-list").height(height-60);
+
 	$.ajax({
 		url:serverUrl+"/users/getClassData",
 		type:"get",
 		dataType:"json",
+		json:"callback",
 		success:function(data){
-			if(data.code==200){
-				var classDoTmpl = doT.template($("#class-tmpl").html());
-                $("#classBox").html(classDoTmpl(data.data));
-
-                $(".classList").click(function(){
-                	$(".classList").removeClass("has-select");
-                	$(this).addClass("has-select");
-                	var text = $(this).text();
-                	getClassListData(text);
-                });
-			}
+			 if(data.code == 200){
+			 	var goodsDoTmpl = doT.template($("#class-tmpl").html());
+                $("#classBox").html(goodsDoTmpl(data.data));
+                if(data.data.length>0){
+                	var firstClassName = data.data[0].className;
+                	getClassDetail(firstClassName);
+                }
+                $(".class-list-name").click(function(){
+					$(".class-list-name").removeClass("active");
+					$(this).addClass("active");
+					var text = $(this).text();
+					getClassDetail(text);
+				});
+			 }
 		},
 		error:function(err){
-			console.log(err);
+
 		}
 	});
 
-	$(".more").click(function(){
-		$(".all").toggle();
-	});
-
-	function getClassListData(text){
-		$(".all").hide();
+	
+	function getClassDetail(text){
 		$.ajax({
 			url:serverUrl+"/users/getClassListData",
 			type:"post",
@@ -41,7 +106,10 @@ $(document).ready(function(){
 					var goodsDoTmpl = doT.template($("#goods-tmpl").html());
                 	$("#goodsBox").html(goodsDoTmpl(data.data));
 
-     
+                	$(".class-detail-container").click(function(){
+                		var id = $(this).attr("data-id");
+                		window.location.href="class_detail.html?id="+id;
+                	});
 				}
 			},
 			error:function(err){
@@ -50,13 +118,3 @@ $(document).ready(function(){
 		});
 	}
 });
-
-function getDetail(obj){
-	
-	if(flag == 0){
-		var id = $(obj).attr("data-id");
-		window.location.href="class_detail.html?id="+id;
-	}
-	flag ++;
-    
-}
